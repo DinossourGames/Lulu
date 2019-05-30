25e924fd0b5bcb8989f09441c0c34a003517e55a
Alef <alef@dinossour.com.br>
2019-05-30 00:06:28 -0300

    Sizes setted to same, for mika work better

--- MainPrototype/MenuScreen.Designer.cs
+++ MainPrototype/MenuScreen.Designer.cs
@@ -35,7 +35,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(294, 135);
+            this.button1.Location = new System.Drawing.Point(505, 149);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(164, 74);
             this.button1.TabIndex = 0;
@@ -45,7 +45,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(294, 236);
+            this.button2.Location = new System.Drawing.Point(505, 250);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(164, 74);
             this.button2.TabIndex = 1;
@@ -55,7 +55,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(294, 336);
+            this.button3.Location = new System.Drawing.Point(505, 350);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(164, 74);
             this.button3.TabIndex = 2;
@@ -67,7 +67,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(813, 481);
+            this.ClientSize = new System.Drawing.Size(1199, 561);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
--- MainPrototype/Tutorial.Designer.cs
+++ MainPrototype/Tutorial.Designer.cs
@@ -34,7 +34,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(644, 373);
+            this.button1.Location = new System.Drawing.Point(1043, 484);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(144, 65);
             this.button1.TabIndex = 0;
@@ -44,9 +44,9 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(12, 12);
+            this.button2.Location = new System.Drawing.Point(22, 12);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(144, 65);
+            this.button2.Size = new System.Drawing.Size(150, 82);
             this.button2.TabIndex = 1;
             this.button2.Text = "Voltar";
             this.button2.UseVisualStyleBackColor = true;
@@ -56,7 +56,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1199, 561);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
